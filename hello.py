565844ff249104622094f3a555df03314911b849
zhitianguan <zguan.university@gmail.com>
2023-09-24 18:51:36 -0400

    Add message flashing when a new name is submitted

--- hello.py
+++ hello.py
@@ -38,9 +38,12 @@ def user(name):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
+    old_name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+        flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'))
