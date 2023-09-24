f1b1bbd9295e687b850207ba0dd2de5eb68673b9
zhitianguan <zguan.university@gmail.com>
2023-09-24 17:46:06 -0400

    Implement redirect and user sessions

--- hello.py
+++ hello.py
@@ -41,6 +41,6 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
