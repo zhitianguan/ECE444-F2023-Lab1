a9b5ba0c27ab31f18346acb9182cf30ce30b8bcd
zhitianguan <zguan.university@gmail.com>
2023-09-24 20:46:22 -0400

    Add field for UofT email address, update template

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -11,9 +11,14 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+def check_utoronto_substring(form, field):
+    email = field.data
+    if 'utoronto' not in email:
+        raise validators.ValidationError('Email address must contain "utoronto".')
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), check_utoronto_substring])
     submit = SubmitField('Submit')
 
 
@@ -25,5 +30,6 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
